2a4634af3617a5360476ee511978af64a7a24ad7
Caelin Hargrave <Caelinhargrave@gmail.com>
2022-07-11 22:47:32 -0500

    Completed lab

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import { Component } from 'react';
+
+
+
+class Header extends Component {
+    constructor() {
+        super();
+    }
+
+
+
+    render() {
+        return (
+            <h1>Horned Animals</h1>
+        )
+    }
+}
+
+export default Header;
